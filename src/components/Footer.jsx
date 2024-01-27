a6ab50a1f3e820525071f61c7eb6faee3018ab63
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2024-01-27 09:46:13 +0700

    feat: Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-black text-white p-3 rounded">
+        <h1 className="text-center font-semibold">
+          Built by Ilham - Api from{" "}
+          <span>
+            <a
+              href="https://github.com/zakiego"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              zakiego
+            </a>
+          </span>
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
+import Footer from "./Footer";
+
 /* eslint-disable react/prop-types */
 const Layout = ({ children }) => {
   return (
     <div className="p-5  flex flex-col justify-center items-center min-h-screen">
       <div className="max-w-[600px] ">
         {children}
-        <div className="mt-3">
-          <div className="bg-black text-white">ini footer</div>
+        <div className="mt-3 ">
+          <Footer />
         </div>
       </div>
     </div>
